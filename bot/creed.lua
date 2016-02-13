28715662ef75c6e3236125dbcc78853bcd305d3a
creeedtest <osaeed48@yahoo.com>
2016-02-13 18:43:31 +0330

    Update creed.lua
    change

--- bot/creed.lua
+++ bot/creed.lua
@@ -236,7 +236,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {118682430},--Sudo users
+    sudo_users = {1165472},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Creed bot 2.3
@@ -245,10 +245,8 @@ function create_config( )
      
     ‼️ this bot is made by : @creed_is_dead
    〰〰〰〰〰〰〰〰
-   ߔࠀ   our admins are : 
-   ߔࠀ   @sorblack_creed
-   ߔࠀ   @amircc_creed
-   ߔࠀ   @aria_creed
+   ߔࠀ   admins are : 
+   ߔࠀ   @ScottmcCall1IR
    〰〰〰〰〰〰〰〰
   ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :
    تمامی درخواست ها و همه ی انتقادات و حرفاتونو با دستور زیر بفرستین به ما
